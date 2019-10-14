375b4c3b3c73951b6636740783e601566d313f7a
chrischne <chrischne@gmail.com>
2019-10-14 19:39:12 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
 var client;
+
+var data = [23,29,20,23,28,23,21,20];
+var index = 0;
 function setup () {
   createCanvas(400, 300);
 
@@ -13,10 +16,14 @@ function setup () {
 
 function draw() { 
   background(255);
-  var payload = '' + floor(random(0, 30));
-  var token = '/hello';
+  var payload = '' + data[index];
+  var token = '/temperature';
   client.publish(token, payload);
   console.log('publish ' + token + ', ' + payload);
 
   text('publish ' + token + ', ' + payload + ' to shiftr.io as ' + client.options.clientId, 100, 100);
+  index++;
+  if(index>data.length-1){
+    index =0;
+  }
 }
