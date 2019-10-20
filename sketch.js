0aa8d3cd1fc6fc011e684550b987e339f3797661
chrischne <chrischne@gmail.com>
2019-10-20 16:31:13 +0200

    load data

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
 var client;
 
-var data = [23,29,20,23,28,23,21,20];
+var data = [];
 var index = 0;
+
+function preload () {
+  data = loadStrings('data.txt');
+}
+
 function setup () {
   createCanvas(400, 300);
+
+
+  //convert strings to numbers
+  for (var i = 0; i < data.length; i++) {
+    data[i] = +data[i];
+  }
+  console.log('data');
+  console.log(data);
 
   client = mqtt.connect('mqtt://42b93b27:68b96339b52a95c5@broker.shiftr.io', {
     clientId: 'vizinviz-faker'
